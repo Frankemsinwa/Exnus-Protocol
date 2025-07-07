33d6f9dd2ddcfdd83508d6f3c2618714375dbfc1
Frank Emesinwa <frankemesinwa@gmail.com>
2025-07-07 00:08:02 +0000

    this is the embed link to the lottie json i told you to add there "https

--- src/components/staking-animation.tsx
+++ src/components/staking-animation.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import Lottie from 'lottie-react';
-import { stakingRewardsAnimation } from '@/lib/animation-data';
 
 export default function StakingAnimation() {
-  return <Lottie animationData={stakingRewardsAnimation} loop={true} className="w-full h-full" />;
+  const animationUrl = "https://lottie.host/8a0f3cda-c59c-4d70-8a17-899b78fe3b81/Z2MgBOWftd.json";
+  return <Lottie path={animationUrl} loop={true} className="w-full h-full" />;
 }
